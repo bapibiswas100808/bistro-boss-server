a0100e21957a61e87a3533510e93becaf12ddb95
bapibiswas100808 <star.bappy@gmail.com>
2024-06-01 12:48:29 +0600

    bistro-boss-server done

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://bistro-boss100.web.app",
+      "https://bistro-boss100.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.5e8b5ac.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -25,7 +35,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const userCollections = client.db("BistroDb").collection("users");
     const menuCollections = client.db("BistroDb").collection("MenuCollections");
@@ -269,7 +279,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
